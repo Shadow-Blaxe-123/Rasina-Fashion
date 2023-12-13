1b9d392220ceeef50aea8450de592bf32b9f3d04
SHAMUN AHMED <Shamyun Ahmed>
2023-12-13 21:46:49 +0600

    Started Work on The Navbar

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import { useState } from 'react';
+import Link from 'next/link';
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <h2>Navbar
+        <nav className="bg-black p-4">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <div className="flex items-center">
+          <Link href="/" className="text-white font-bold text-lg">Logo</Link>
+          
+        </div>
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="text-white focus:outline-none focus:text-white"
+          >
+            <svg
+              className="h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {isOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+        
+        <div className={`md:flex ${isOpen ? 'block' : 'hidden'}`}>
+
+          <div className="flex flex-col md:flex-row md:items-center md:space-x-4">
             
-        </h2>
+            <Link href="/" className="text-white hover:text-gray-300">Home</Link>
+            <Link href="/about" className="text-white hover:text-gray-300">About</Link>
+            <Link href="/contact" className="text-white hover:text-gray-300">Contact Us</Link>
+            {/* Add more links as needed */}
+          </div>
+        </div>
+      </div>
+    </nav>
     </div>
   )
 }

ce8163334c09e82e33c680696698054f824c75fa
SHAMUN AHMED <Shamyun Ahmed>
2023-12-16 22:15:54 +0600

    DOne with Footer but I need to rebuild Navbar (maybe).

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -12,14 +12,14 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gray-800 text-white top-0 fixed w-full mb-8 -z-10">
+    <nav className="nav-overlay font-bold text-white top-0 fixed w-full mb-8 -z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
               {/* Your logo or site name */}
 
-              <Link href="/" className="text-white">
+              <Link href="/" className="text-white hidden md:block">
                 Your Logo
               </Link>
             </div>

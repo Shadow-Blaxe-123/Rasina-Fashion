1c726e98499f05ae3e2749150b3c39a994158184
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2023-12-19 22:27:33 +0600

    Done with the Home Page...

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
-import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <div id="home" className="h-screen">
-        Welcome to my site.
+      <div
+        id="home"
+        className="h-screen -z-10 flex align-middle justify-center"
+      >
+        <div className="text-white  z-10 m-auto ">
+          <h2 className="font-black text-6xl border-b-2 border-green-500">
+            Welcome to Rasina Fashion.
+          </h2>
+          <br />
+          <p className="flex align-middle justify-center text-3xl">
+            We are knit garments manufacturer & exporter
+          </p>
+          <br />
+          <div className="flex">
+            <Link
+              href={"/about"}
+              className="m-auto text-xl bg-red-500 p-3 rounded-lg border-black border-2"
+            >
+              About Us
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
